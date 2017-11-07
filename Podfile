8714315d347b9e4edbd55b8cbd45e66fab88ca63
sgr-ksmt <suguru-kishimoto@cookpad.com>
2017-11-08 00:49:24 +0900

    initial commit.

---
+++ Podfile
@@ -0,0 +1,8 @@
+platform :ios, '9'
+
+inhibit_all_warnings!
+use_frameworks!
+
+target 'RxFirebaseAuth' do
+  pod 'Firebase/Auth'
+end
---
+++ RxFirebaseAuth/RxFirebaseAuth.h
@@ -0,0 +1,19 @@
+//
+//  RxFirebaseAuth.h
+//  RxFirebaseAuth
+//
+//  Created by suguru-kishimoto on 2017/11/08.
+//  Copyright © 2017年 Suguru Kishimoto. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RxFirebaseAuth.
+FOUNDATION_EXPORT double RxFirebaseAuthVersionNumber;
+
+//! Project version string for RxFirebaseAuth.
+FOUNDATION_EXPORT const unsigned char RxFirebaseAuthVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RxFirebaseAuth/PublicHeader.h>
+
+
