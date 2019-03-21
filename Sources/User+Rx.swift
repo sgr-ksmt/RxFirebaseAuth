c4a4b943c6822542fec31e51be14b76f88707c3c
sgr-ksmt <suguru-kishimoto@cookpad.com>
2019-03-21 14:10:26 +0900

    refactor overall.

--- Sources/User+Rx.swift
+++ Sources/User+Rx.swift
@@ -13,16 +13,16 @@ import RxSwift
 
 // MARK: - Reload / re-authenticate
 extension Reactive where Base: User {
-    public func reload() -> Single<Void> {
-        return .create { [weak user = self.base] observer in
-            user?.reload(completion: singleEventErrorHandler(observer))
+    public func reload() -> Completable {
+        return .create { [weak user = base] in
+            user?.reload(completion: completableEventHandler($0))
             return Disposables.create()
         }
     }
 
     public func reauthenticateAndRetrieveData(with credential: AuthCredential) -> Single<AuthDataResult> {
-        return .create { [weak user = self.base] observer in
-            user?.reauthenticateAndRetrieveData(with: credential, completion: singleEventHandler(observer))
+        return .create { [weak user = base] in
+            user?.reauthenticateAndRetrieveData(with: credential, completion: singleEventHandler($0))
             return Disposables.create()
         }
     }
@@ -31,33 +31,32 @@ extension Reactive where Base: User {
 // MARK: - link/unlink
 extension Reactive where Base: User {
     public func linkAndRetrieveData(with credential: AuthCredential) -> Single<AuthDataResult> {
-        return .create { [weak user = self.base] observer in
-            user?.linkAndRetrieveData(with: credential, completion: singleEventHandler(observer))
+        return .create { [weak user = base] in
+            user?.linkAndRetrieveData(with: credential, completion: singleEventHandler($0))
             return Disposables.create()
         }
     }
 
     public func unlink(with provider: String) -> Single<User> {
-        return .create { [weak user = self.base] observer in
+        return .create { [weak user = base] observer in
             user?.unlink(fromProvider: provider, completion: singleEventHandler(observer))
             return Disposables.create()
         }
     }
 }
 
-
 // MARK: - Update e-mail, password
 extension Reactive where Base: User {
     public func updateEmail(to email: String) -> Single<Void> {
-        return .create { [weak user = self.base] observer in
+        return .create { [weak user = base] observer in
             user?.updateEmail(to: email, completion: singleEventErrorHandler(observer))
             return Disposables.create()
         }
     }
 
-    public func updatePassword(to password: String) -> Single<Void> {
-        return .create { [weak user = self.base] observer in
-            user?.updatePassword(to: password, completion: singleEventErrorHandler(observer))
+    public func updatePassword(to password: String) -> Completable {
+        return .create { [weak user = base] in
+            user?.updatePassword(to: password, completion: completableEventHandler($0))
             return Disposables.create()
         }
     }
@@ -65,16 +64,16 @@ extension Reactive where Base: User {
 
 // MARK: - Send e-mail verification
 extension Reactive where Base: User {
-    public func sendEmailVerification() -> Single<Void> {
-        return .create { [weak user = self.base] observer in
-            user?.sendEmailVerification(completion: singleEventErrorHandler(observer))
+    public func sendEmailVerification() -> Completable {
+        return .create { [weak user = base] in
+            user?.sendEmailVerification(completion: completableEventHandler($0))
             return Disposables.create()
         }
     }
 
-    public func sendEmailVerification(with actionCodeSettings: ActionCodeSettings) -> Single<Void> {
-        return .create { [weak user = self.base] observer in
-            user?.sendEmailVerification(with: actionCodeSettings, completion: singleEventErrorHandler(observer))
+    public func sendEmailVerification(with actionCodeSettings: ActionCodeSettings) -> Completable {
+        return .create { [weak user = base] in
+            user?.sendEmailVerification(with: actionCodeSettings, completion: completableEventHandler($0))
             return Disposables.create()
         }
     }
@@ -83,28 +82,28 @@ extension Reactive where Base: User {
 // MARK: - Get ID token
 extension Reactive where Base: User {
     public func getIDToken() -> Single<String> {
-        return .create { [weak user = self.base] observer in
+        return .create { [weak user = base] observer in
             user?.getIDToken(completion: singleEventHandler(observer))
             return Disposables.create()
         }
     }
 
     public func getIDTokenResult() -> Single<AuthTokenResult> {
-        return .create { [weak user = self.base] observer in
+        return .create { [weak user = base] observer in
             user?.getIDTokenResult(completion: singleEventHandler(observer))
             return Disposables.create()
         }
     }
 
     public func getIDTokenForcingRefresh(_ forceRefresh: Bool) -> Single<String> {
-        return .create { [weak user = self.base] observer in
+        return .create { [weak user = base] observer in
             user?.getIDTokenForcingRefresh(forceRefresh, completion: singleEventHandler(observer))
             return Disposables.create()
         }
     }
 
     public func getIDTokenResult(forcingRefresh: Bool) -> Single<AuthTokenResult> {
-        return .create { [weak user = self.base] observer in
+        return .create { [weak user = base] observer in
             user?.getIDTokenResult(forcingRefresh: forcingRefresh, completion: singleEventHandler(observer))
             return Disposables.create()
         }
@@ -113,9 +112,9 @@ extension Reactive where Base: User {
 
 // MARK: - Delete user
 extension Reactive where Base: User {
-    public func delete() -> Single<Void> {
-        return .create { [weak user = self.base] observer in
-            user?.delete(completion: singleEventErrorHandler(observer))
+    public func delete() -> Completable {
+        return .create { [weak user = base] in
+            user?.delete(completion: completableEventHandler($0))
             return Disposables.create()
         }
     }
